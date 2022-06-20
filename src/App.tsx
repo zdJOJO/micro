e3186bcf651662deb31c6a9dad7b6d66769ad61b
zhangd <zhangd@ibuscloud.com>
2022-06-20 13:51:16 +0800

    🐞 fix: 修改demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React, { Suspense } from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
+// import { BrowserRouter as RouterV5, Switch, Route } from 'react-router-dom-v5'
 import Routes from './route'
 import './App.less'
 
+/**
+ *  react-router-dom V6版本
+ *
+ */
 function App() {
   return (
     <div className="App">
